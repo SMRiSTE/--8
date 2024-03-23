13f4ded1ef38b4627bb2e93df1088301f44d9c03
Ш.М.Р. <s.m.r.00800ist.@gmail.com>
2024-03-23 20:38:37 +0300

    Initial commit

---
+++ 2/2/2.cpp
@@ -0,0 +1,108 @@
+﻿#include <iostream>
+
+class Fraction {
+private:
+    int numerator;
+    int denominator;
+
+public:
+    Fraction(int num = 0, int den = 1){
+        numerator = num;
+        denominator = den;
+    }
+
+    Fraction operator+(const Fraction& other) {
+        return Fraction(numerator * other.denominator + other.numerator * denominator, denominator * other.denominator);
+    }
+
+    Fraction operator-(const Fraction& other) {
+        return Fraction(numerator * other.denominator - other.numerator * denominator, denominator * other.denominator);
+    }
+
+    Fraction operator*(const Fraction& other) {
+        return Fraction(numerator * other.numerator, denominator * other.denominator);
+    }
+
+    Fraction operator/(const Fraction& other) {
+        return Fraction(numerator * other.denominator, denominator * other.numerator);
+    }
+
+    Fraction operator-() {
+        return Fraction(-numerator, denominator);
+    }
+
+    Fraction& operator++() {
+        numerator += denominator;
+        return *this;
+    }
+
+    Fraction operator++(int) {
+        Fraction temp = *this;
+        numerator += denominator;
+        return temp;
+    }
+
+    Fraction& operator--() {
+        numerator -= denominator;
+        return *this;
+    }
+
+    Fraction operator--(int) {
+        Fraction temp = *this;
+        numerator -= denominator;
+        return temp;
+    }
+
+    void print() {
+        std::cout << numerator << '/' << denominator << std::endl;
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    int num1, den1, num2, den2;
+    std::cout << "Введите числитель дроби 1: ";
+    std::cin >> num1;
+    std::cout << "Введите числитель дроби 1: ";
+    std::cin >> den1;
+
+    std::cout << "Введите знаменатель дроби 2: ";
+    std::cin >> num2;
+    std::cout << "Введите знаменатель дроби 2: ";
+    std::cin >> den2;
+
+    Fraction f1(num1, den1);
+    Fraction f2(num2, den2);
+
+    Fraction sum = f1 + f2;
+    Fraction sub = f1 - f2;
+    Fraction mult = f1 * f2;
+    Fraction div = f1 / f2;
+
+    std::cout << "f1 + f2 = ";
+    sum.print();
+
+    std::cout << "f1 - f2 = ";
+    sub.print();
+
+    std::cout << "f1 * f2 = ";
+    mult.print();
+
+    std::cout << "f1 / f2 = ";
+    div.print();
+
+    std::cout << "++f1 * f2 = ";
+    (++f1).print();
+
+    std::cout << "Value of f1 = ";
+    f1.print();
+
+    std::cout << "f1-- * f2 = ";
+    (f1-- * f2).print();
+
+    std::cout << "Value of f1 = ";
+    f1.print();
+
+    return 0;
+}
